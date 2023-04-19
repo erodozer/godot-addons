72e0338695cc1e0d6d3fb93a56f4dec34f843307
Erodozer <ero@erodozer.moe>
2023-04-19 18:31:44 -0400

    [ssm] update for godot 4

--- simple-scene-manager/SceneManager.gd
+++ simple-scene-manager/SceneManager.gd
@@ -1,6 +1,6 @@
 """
 SceneManager.gd
-@author Nicholas Hydock <nhydock@gmail.com>
+@author Erodozer <ero@erodozer.moe>
 @description |>
 	Simplify complex scene management with safe memory transitioning
 	and dynamic loading of Node.  Scenes are the primary composition of a
@@ -11,9 +11,10 @@ SceneManager.gd
 	tied to when scenes load, unload, and transition into focus.
 """
 extends Node
+class_name SceneManager
 
-onready var anim = $Transition/Fader/AnimationPlayer
-onready var container = $Scene
+@onready var anim = %AnimationPlayer
+@onready var container = %Scene
 
 var current_scene
 var is_active = false
@@ -21,27 +22,10 @@ var is_active = false
 signal setup
 signal started
 
-static func load_async(path):
-	yield(Engine.get_main_loop(), "idle_frame")
-	var loader = ResourceLoader.load_interactive(path)
-	while loader.poll() == OK:
-		yield(Engine.get_main_loop(), "idle_frame")
-	
-	assert(loader.poll() == ERR_FILE_EOF)
-	return loader.get_resource()
-
-func _notification(what: int) -> void:
-	match what:
-		MainLoop.NOTIFICATION_WM_FOCUS_OUT:
-			Engine.target_fps = 10
-		MainLoop.NOTIFICATION_WM_FOCUS_IN:
-			# Disable the FPS cap
-			Engine.target_fps = 0
-
 func _ready():
 	if container.get_child_count() == 0:
 		# force wait until current scene is ready
-		yield(get_tree(), "idle_frame")
+		await get_tree().process_frame
 		var s = get_tree().current_scene
 		if s.is_in_group("scene"):
 			s.get_parent().remove_child(s)
@@ -49,7 +33,10 @@ func _ready():
 	else:
 		change_scene(container.get_child(0))
 
-func change_scene(next_scene, params=[]):
+static func change_scene(next_scene, params=[]):
+	Engine.get_main_loop().get_nodes_in_group('scene_manager')[0]._change_scene(next_scene, params)
+
+func _change_scene(next_scene, params=[]):
 	"""
 	Changes scene with a transition.
 	You can hold the fade back in transition if desired until
@@ -59,17 +46,15 @@ func change_scene(next_scene, params=[]):
 	is_active = true
 	
 	anim.play("Fade")
-	yield(anim, "animation_finished")
+	await anim.animation_finished
 	
 	var scene = current_scene
 	if scene != null:
 		if scene.has_method("_teardown"):
-			var state = scene._teardown()
-			if state and state is GDScriptFunctionState:
-				yield(state, "completed")
-		yield(get_tree(), "idle_frame")
+			await scene._teardown()
+		await get_tree().process_frame
 		scene.queue_free()
-		yield(scene, "tree_exited")
+		await scene.tree_exited
 	
 	if next_scene is String:
 		"""
@@ -77,30 +62,28 @@ func change_scene(next_scene, params=[]):
 		we only need to fade in.  Else we're loading the scene
 		"""
 		next_scene = next_scene if next_scene.begins_with("res://") else "res://scenes/%s/scene.tscn" % next_scene
-		var res = yield(load_async(next_scene), "completed")
-		current_scene = res.instance()
+		var res = await ResourceLoader.load(next_scene)
+		current_scene = res.instantiate()
 	elif next_scene is Node:
 		current_scene = next_scene
 	else:
-		current_scene = next_scene.instance()
+		current_scene = next_scene.instantiate()
 		
 	container.add_child(current_scene)
 		
 	if current_scene.has_method("_setup"):
-		var state = current_scene._setup(params)
-		if state and state is GDScriptFunctionState:
-			yield(state, "completed")
-	emit_signal("setup")
+		await current_scene._setup(params)
+		
+	setup.emit()
 	
 	anim.play_backwards("Fade")
-	yield(anim, "animation_finished")
+	await anim.animation_finished
 	
 	get_tree().paused = false
 	
 	if current_scene.has_method("_start"):
-		var state = current_scene._start()
-		if state and state is GDScriptFunctionState:
-			yield(state, "completed")
-	emit_signal("started")
+		await current_scene._start()
+		
+	started.emit()
 	
 	is_active = false
