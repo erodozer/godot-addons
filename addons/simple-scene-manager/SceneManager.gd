bde2cd4f065110bc05f1abe592340f17af71d939
Erodozer <ero@erodozer.moe>
2023-12-18 19:31:34 -0500

    [ssm] redesign, prefer as autoload singleton

--- addons/simple-scene-manager/SceneManager.gd
+++ addons/simple-scene-manager/SceneManager.gd
@@ -1,70 +1,107 @@
-"""
-SceneManager.gd
-@author Erodozer <ero@erodozer.moe>
-@description |>
-	Simplify complex scene management with safe memory transitioning
-	and dynamic loading of Node.  Scenes are the primary composition of a
-	game's stateful context.  
-	SceneManager introduces additional lifecycle hooks that are useful
-	for Scenes that extend the functionality of Godot's own Node lifecycle.
-	These new hooks are fully optional methods and allow for activity
-	tied to when scenes load, unload, and transition into focus.
-"""
 extends Node
-class_name SceneManager
+class_name SimpleSceneManager
+## Simplify complex scene management with safe memory transitioning
+## and threaded loading of Nodes.
+##
+## This library is designed to supercede usage of Godot's built-in 
+## current_scene functionality that is baked into the SceneTree.[br]
+## SceneManager introduces additional lifecycle hooks that are useful
+## for Scenes that extend the functionality of Godot's own Node lifecycle.
+## These new hooks are fully optional methods and allow for activity
+## tied to when scenes load, unload, and transition into focus.
+## 
+## [br][br]
+## Order of hooks
+## [codeblock]
+## - _tree_entered
+## - _ready
+## - _setup (async)
+##  (fade out)
+## - _start (async)
+## - _teardown (async)
+##  (fade in)
+## - _tree_exited
+## [/codeblock]
+##
+## As noted, for added convenience all the new lifecycle nodes that scenes have allow for coroutines.
+## This allows for non-blocking processing and loading screens.
+## [br]
+## To use SceneManager, it is recommended to add SceneManager.tscn to your project's Autoload.
+## It has built-in behavior to work with single scene testing as well, so that all lifecycle hooks
+## can be properly evoked, as long as the scene is properly tagged with the group name of "scene".
+## [br]
+## If you wish to customize SceneManager, such as the dither texture, animation speed, or more,
+## it is recommended to create an InstancedScene derived from the SceneManager.tscn, and then
+## assign that to your AutoLoad.[br]
+## [br]
+## @author: Erodozer <ero@erodozer.moe> [br]
+## @version: 3.0.0 [br]
+## @godot_version: [min 4.0] [br]
 
-@onready var anim = %AnimationPlayer
-@onready var container = %Scene
-
-var current_scene
-var is_active = false
-
+## Signal emitted after scene _setup hook has been completed
 signal setup
+## Signal emitted after scene _start hook has been completed
 signal started
+## Signal emitted after scene _teardown hook has been completed
+signal teardown
 
 func _ready():
+	var container = %Scene
 	if container.get_child_count() == 0:
 		# force wait until current scene is ready
 		await get_tree().process_frame
 		var s = get_tree().current_scene
 		if s.is_in_group("scene"):
 			s.get_parent().remove_child(s)
-			_change_scene(s)
+			change_scene(s)
 	else:
 		var scene = container.get_child(0)
 		container.remove_child(scene)
-		_change_scene(scene)
+		change_scene(scene)
 
-static func change_scene(next_scene, params=[]):
-	Engine.get_main_loop().get_nodes_in_group('scene_manager')[0]._change_scene(next_scene, params)
-
-func _change_scene(next_scene, params=[]):
-	"""
-	Changes scene with a transition.
-	You can hold the fade back in transition if desired until
-	the next scene is in a state that is considered ready
-	"""
+## Changes scene with a transition.
+## You can hold the fade back in transition if desired until
+## the next scene is in a state that is considered ready
+##
+## Parameters passed in are fed into the _setup lifecycle hook
+func change_scene(next_scene, params=[]):
 	get_tree().paused = true
-	is_active = true
 	
-	var scene = current_scene
-	if scene != null:
+	var anim = %AnimationPlayer
+	var container = %Scene
+	var current_scene = container.get_child(-1)
+	if current_scene != null:
+		if current_scene.has_method("_teardown"):
+			await current_scene._teardown()
+			
+		teardown.emit()
+			
 		anim.play("Fade")
 		await anim.animation_finished
 		
-		if scene.has_method("_teardown"):
-			await scene._teardown()
+		# if any additional processing should happen after fade-out
+		# use the built-in tree_exited hook
+		
 		await get_tree().process_frame
-		scene.queue_free()
-		await scene.tree_exited
+		current_scene.queue_free()
+		await current_scene.tree_exited
 	
 	if next_scene is String:
-		"""
-		if a scene node is passed through, like with debugging,
-		we only need to fade in.  Else we're loading the scene
-		"""
+		# if a scene node is passed through, like with debugging,
+		# we only need to fade in.  Else we're loading the scene
 		next_scene = next_scene if next_scene.begins_with("res://") else "res://scenes/%s/scene.tscn" % next_scene
-		var res = await ResourceLoader.load(next_scene)
+		var loader = ResourceLoader.load_threaded_request(next_scene)
+		var res = null
+		while res == null:
+			match ResourceLoader.load_threaded_get_status(next_scene):
+				ResourceLoader.THREAD_LOAD_LOADED:
+					res = ResourceLoader.load_threaded_get(next_scene)
+				ResourceLoader.THREAD_LOAD_IN_PROGRESS:
+					continue
+				_:
+					assert(false, "Failed to load scene, this should never happen")
+					return false
+				
 		current_scene = res.instantiate()
 	elif next_scene is Node:
 		current_scene = next_scene
@@ -87,5 +124,3 @@ func _change_scene(next_scene, params=[]):
 		await current_scene._start()
 		
 	started.emit()
-	
-	is_active = false
