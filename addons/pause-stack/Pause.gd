d5bde0b34efe9cc85907d76274e14c2b3806e027
erodozer <ero@erodozer.moe>
2026-06-13 01:47:53 -0400

    [pause] update to godot 4, prefer set instead of pure stack model

--- addons/pause-stack/Pause.gd
+++ addons/pause-stack/Pause.gd
@@ -14,25 +14,29 @@ PauseStack.gd
 """
 extends Node
 
-var pause_stack = []
+var registry = []
 
+## signal emitted whenever a new pause state is added to the stack
 signal paused(reason)
-signal resumed()
 
-var is_paused setget ,get_paused
+## signal emitted when the stack is completely clear
+signal resumed
 
-func get_paused():
-	return len(pause_stack) > 0
+func is_paused(reason = "") -> bool:
+	if reason.is_empty():
+		return not registry.is_empty()
+	return reason in registry
 
-func pause(reason = 'game.paused'):
+func pause(reason: String = 'game.paused'):
 	var is_paused = self.is_paused
-	assert(not(reason in pause_stack), "incorrect state in pause stack, reason can not be present in the stack multiple times.  Duplicate: %s" % reason)
-	pause_stack.append(reason)
-	if not is_paused:
-		emit_signal("paused", reason)
+	assert(reason not in registry, "incorrect state in pause stack, reason can not be present in the stack multiple times.  Duplicate: %s" % reason)
+	registry.append(reason)
+	paused.emit(reason)
 	
-func resume(reason):
-	var source = pause_stack.pop_back()
-	assert(source == reason, "incorrect state from pause stack, fix your cleanup. Expected %s, Found %s" % [reason, source])
-	if !get_paused():
-		emit_signal("resumed")
+func resume(reason: String):
+	assert(reason in registry, "incorrect state from pause stack, reason is missing.  Reason %s" % reason)
+	
+	registry.erase(reason)
+	
+	if not is_paused():
+		resumed.emit()
